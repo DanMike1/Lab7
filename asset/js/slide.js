4bd6f1cb95e3ead6f587d2898b9a62c3e1cc80a9
DanMike1 <50786839+DanMike1@users.noreply.github.com>
2021-02-14 16:18:32 +0300

    done with slider with carousel

--- asset/js/slide.js
+++ asset/js/slide.js
@@ -1,61 +1,36 @@
-const slide = document.getElementById("meSlide")
-const prev = document.getElementById("prev")
-const next = document.getElementById("next")
+ const slide1 = document.getElementById("meSlide1")
+ const slide2 = document.getElementById("meSlide2")
+ const slide3 = document.getElementById("meSlide3")
+$('.carousel').carousel({
+    interval: 20000
+});
 
-prev.addEventListener('click',thotDan)
-next.addEventListener('click',thotDan)
+$('.carousel').on('slid.bs.carousel',async function (e) {
+    if (slide1){
+            let geturl = await fetch('https://picsum.photos/1024/767');
+            let data =  geturl.url;
+            let imag = document.querySelector(".carousel-item.active").querySelector("img");
+            imag.src = data;
+    }
 
-// function prevPhoto(){
-//     fetch("https://picsum.photos/1080/720")
-//         .then(res=>res.json())
-//         .then(data =>{
+    else if (slide2){
+            let geturl = await fetch('https://picsum.photos/1024/766');
+            let data =  geturl.url;
+            let imag = document.querySelector(".carousel-item.active").querySelector("img");
+            imag.src = data;
+    }
 
-           
 
-//         })
-
-// }
-// function nextPhoto(){
-//     fetch("https://picsum.photos/300/720")
-//         .then(res=>res.json())
-//         .then(data =>{
-//             meSlide.innerHTML = <img src = "${data.file}"/>
-
-//         })
-
-// }
-async function thotDan(){
-    let response = await fetch('https://picsum.photos/1024/768');
-    let data = await response.url;
-    let imag = document.getElementById("img_")
-    imag.src = data
+    else if (slide3){
+            let geturl = await fetch('https://picsum.photos/1024/765');
+            let data =  geturl.url;
+            let imag = document.querySelector(".carousel-item.active").querySelector("img");
+            imag.src = data;
     
 
-    // imag.src= response
-}
-// var slideIndex = 1;
-// showSlides(slideIndex);
+    }
 
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
+  
+});
 
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}    
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";  
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";  
-//   dots[slideIndex-1].className += " active";
-// }
+    
